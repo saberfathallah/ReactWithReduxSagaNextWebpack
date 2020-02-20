c5033e4515beaa39be73848e99d562bd26df6260
saber <saberfathallah7@gmail.com>
2020-02-20 11:49:50 +0100

    learn2

--- react-redux-saga-next-webpack/src/redux/reducers.js
+++ react-redux-saga-next-webpack/src/redux/reducers.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 
 import usersReducer from "./users/reducer";
 console.log("branche learn");
+console.log("branche learn2");
 const rootReducer = combineReducers({
   usersReducer,
 });
