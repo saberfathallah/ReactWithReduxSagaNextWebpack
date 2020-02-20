74c3af31a16bf015986c287e63d7648363b5ae9b
saber <saberfathallah7@gmail.com>
2020-02-20 11:45:49 +0100

    master 1

--- react-redux-saga-next-webpack/src/redux/sagas.js
+++ react-redux-saga-next-webpack/src/redux/sagas.js
@@ -1,7 +1,7 @@
 import { all, fork } from "redux-saga/effects";
 
 import usersSaga from "./users/sagas";
-
+console.log("master 1");
 export default function* rootSaga() {
   yield all([fork(usersSaga)]);
 }
